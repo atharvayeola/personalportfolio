f2719a8641bd7e089169de0c989dfeb0b4f91369
Atharva Yeola <atharvayeola12@gmail.com>
2025-04-07 13:53:09 -0700

    updated details

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,13 @@ const About = () => {
       )}
 
       {role && <h2 className='about__role'> {role}</h2>}
-      <p className='about__desc'>{description && description}</p>
+      {<ul className='about__desc'>
+  {description &&
+    description.map((point) => (
+      <li key={point} dangerouslySetInnerHTML={{ __html: point }} />
+    ))}
+</ul>
+/* <p className='about__desc'>{description && description}</p> */}
 
       <div className='about__contact center'>
         {resume && (
--- src/portfolio.js
+++ src/portfolio.js
@@ -7,10 +7,21 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Atharva Yeola',
-  role: 'Machine Learning Engineer',
-  description:
-    'A software engineer with expertise in machine learning, computer vision and LLMs. Prof. Bintu (his PI @UCSD) quotes "Atharva is proficient in Python and PyTorch, with experience in writing advanced machine learning algorithms and designing data processing pipelines." His manager @Rhythmflows Solutions Pvt. Ltd says - "Atharva has demonstrated success in understanding business needs, managing complex projects, and enhancing model performance with cutting-edge techniques." Mr Mokashi (Director @Atomic Loops) mentions: "Atharva has proven ability to lead teams and collaborate effectively in high-impact projects."',
-  resume: '/resume.pdf',
+  role: 'Data Scientist',
+  description: [
+    '→ A data scientist with expertise in analytics, decision science, machine learning, and LLMs.',
+    '→ <a href="https://www.johnwayers.com/" target="_blank" rel="noopener noreferrer">Dr. John Ayers</a> (his PI at <a href="https://ucsd.edu" target="_blank" rel="noopener noreferrer">UCSD</a>) quotes: "Atharva is a Masters student at UCSD but easily mistaken for a senior PhD fellow."',
+    '→ He is a wonderful addition to our innovation ecosystem at <a href="https://qi.ucsd.edu/" target="_blank" rel="noopener noreferrer">Qualcomm Institute (QI)</a>, bridging science and practice!',
+    '→ In less than a year, Atharva published a study in <a href="https://jamanetwork.com/journals/jamainternalmedicine/fullarticle/2830019" target="_blank" rel="noopener noreferrer">JAMA Internal Medicine</a> (4% acceptance rate), gaining significant media attention.',
+    'His work has been featured in several prominent media outlets, including:',
+    '<ul>',
+    '<li><a href="https://nypost.com/2025/02/18/sports/gambling-addiction-soaring-since-legalization-of-sports-betting-in-most-us-states/" target="_blank" rel="noopener noreferrer">New York Post</a></li>',
+    '<li><a href="https://www.cbsnews.com/news/gambling-addiction-online-search-surge-sports-betting/" target="_blank" rel="noopener noreferrer">CBS News</a></li>',
+    '<li><a href="https://www.nbcnews.com/health/health-news/online-searches-gambling-addiction-surge-legalized-sports-betting-expa-rcna192462" target="_blank" rel="noopener noreferrer">NBC News</a></li>',
+    '<li><a href="https://www.npr.org/sections/shots-health-news/2025/02/18/nx-s1-5299943/as-sports-betting-has-soared-more-people-search-online-for-help-with-gambling-addiction" target="_blank" rel="noopener noreferrer">NPR</a></li>',
+    '</ul>',
+    '→ <a href="https://hivtrends.org" target="_blank" rel="noopener noreferrer">HIVTrends.org</a>—a collaboration of <a href="https://cfar.ucsd.edu" target="_blank" rel="noopener noreferrer">CFAR</a>, <a href="https://www.jhu.edu/" target="_blank" rel="noopener noreferrer">JHU</a>, <a href="https://qi.ucsd.edu" target="_blank" rel="noopener noreferrer">QI</a>, and <a href="https://actri.ucsd.edu" target="_blank" rel="noopener noreferrer">ACTRI</a>—was made possible through his key contributions.'
+  ],  resume: '/resume.pdf',
   social: {
     linkedin: 'https://linkedin.com/in/atharvayeola',
     github: 'https://github.com/atharvayeola',
@@ -44,22 +55,44 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'Python', 'Computer Vision', 'LLMs', 'SQL', 'Pytorch', 'TensorFlow', 'PySpark', 'AWS', 'Docker'
+  'Python', 'SQL', 'Spark', 'AWS', 'Docker', 'LLMs', 'PyTorch'
 ]
-
 const news = [
   {
-    title: 'Paper Accepted at IEEE CVMI 2024 Conference',
+    title: 'Original Investigation in JAMA Internal Medicine',
     description: {
-      text: 'My paper titled "A Deep Learning-based Study on Occluded Traffic Signs" (ID 552) was accepted at the 3rd IEEE CVMI 2024 conference. The conference will be held on October 19-20, 2024, in Prayagraj, India. This paper elaborates on innovative approaches to interpreting traffic signs that are partially or fully occluded, a challenge in the field of computer vision.',
+      text: 'Our study titled "Growing Health Concern Regarding Gambling Addiction in the Age of Sportsbooks" was published in JAMA Internal Medicine. This research examines the association between the legalization of sports betting in the U.S. and the increase in gambling addiction help-seeking behaviors.',
       link: {
-        url: 'https://cvmi2024.iiita.ac.in/AcceptedPapers.php',
-        text: 'A Deep Learning-based Study on Occluded Traffic Signs',
+        url: 'https://jamanetwork.com/journals/jamainternalmedicine/article-abstract/2830019',
+        text: 'Growing Health Concern Regarding Gambling Addiction in the Age of Sportsbooks',
       },
     },
-    date: 'August 1, 2024',
+    date: 'February 17, 2025',
   },
-]
+  {
+    title: 'Launch of HIVTrends.org',
+    description: {
+      text: 'Collaborated on the development of HIVTrends.org, a platform providing public, real-time, and validated HIV testing sales trends from search query surveillance. This project aims to enhance public health monitoring and response strategies.',
+      link: {
+        url: 'https://hivtrends.org',
+        text: 'HIVTrends.org',
+      },
+    },
+    date: 'December 20, 2024',
+  },
+  {
+    title: 'Research Study at IEEE Xplore (CVMI 2024)',
+    description: {
+      text: 'Our paper titled "Enhancing Traffic Sign Recognition: A Deep Learning Approach for Occluded Environments" was published in IEEE Xplore and presented at the 3rd IEEE CVMI 2024 conference.',
+      link: {
+        url: 'https://ieeexplore.ieee.org/document/10782104',
+        text: 'Enhancing Traffic Sign Recognition: A Deep Learning Approach for Occluded Environments',
+      },
+    },
+    date: 'December 11, 2024',
+  },
+];
+
 
 const blogposts = [
   {
